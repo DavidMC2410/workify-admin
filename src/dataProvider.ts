1410ee89d1f2b2062a376917c0a7921d6efb6b7e
DAMC <david.moreno.cardenas@gmail.com>
2023-06-14 14:35:30 -0400

    Connert to DB

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -3,7 +3,7 @@ import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 import { log } from 'console';
 
-const apiUrl = 'https://domesticservicesbackend-production-acb6.up.railway.app/';
+const apiUrl = 'https://domesticservicesbackend-production-acb6.up.railway.app';
 //const apiUrl = 'http://localhost:3001';
 const httpClient = fetchUtils.fetchJson;
 
